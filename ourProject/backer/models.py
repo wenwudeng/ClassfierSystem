913b32562c4eaea2e4c4a4a65e2b5b7f6a74d65c
PrayWind <515322309@qq.com>
2019-09-10 18:31:47 +0800

    增加了数据库增删改查的例子（Person表）

--- ourProject/backer/models.py
+++ ourProject/backer/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
 
+# 创建user表
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=11)
+    password = models.CharField(max_length=20)
+    type = models.CharField(max_length=20)
+
+    # 修改表名
+    class Meta:
+        db_table = "T_User"
+
+
 # 创建person表
 class Person(models.Model):
     id = models.AutoField(primary_key=True)
