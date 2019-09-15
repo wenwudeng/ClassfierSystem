24320f7e411451777a09f7656564556f3ced53e7
zc-zack <1301842136@qq.com>
2019-09-16 00:17:02 +0800

    爬虫前台未完成

--- ourProject/fonter/src/components/page/Reptile.vue
+++ ourProject/fonter/src/components/page/Reptile.vue
@@ -1,10 +1,41 @@
 <template>
-  <div>爬虫</div>
+  <div>
+    <div>爬虫</div>
+    <div>
+      <select v-model="url_value">
+        <option value="1">百度</option>
+        <option value="2">搜狗</option>
+      </select>
+      <input v-model="word"/>
+      <button @click="reptile">爬虫</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  name: "Reptile",
+  data(){
+    return{
+      img_list: [],
+    };
+  },
 
+  methods: {
+    reptile(){
+      console.log(this.word);
+      this.$axios
+      .post('/reptile/', {
+        url_value: this.url_value,
+        word: this.word
+      })
+      .then(response => {
+
+      })
+      .catch(error => {});
+
+    }
+  }
 }
 </script>
 
