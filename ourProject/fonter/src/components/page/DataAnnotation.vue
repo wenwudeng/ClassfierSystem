173dba897da5ba8714a50c585c3935c70f4f990e
Gray <1849307507@qq.com>
2019-09-16 14:36:45 +0800

    数据标注1

--- ourProject/fonter/src/components/page/DataAnnotation.vue
+++ ourProject/fonter/src/components/page/DataAnnotation.vue
@@ -1,12 +1,12 @@
 <template>
-  <div clas="outer">
+  <div clas="outer" style="text-align: center">
     <!--图片显示-->
     <el-row :gutter="20">
       <el-col :span="6" v-for="(i,index) in srcList" :key="index">
         <el-image
-          style="width: 300px;height:250px;"
+          style="width: 300px;height:250px;margin-top: 20px"
           :src='i.url'
-          :preview-src-list="srcList">
+        >
         </el-image>
         <div style="margin-top: 3px">
           <!--选择器-->
@@ -15,7 +15,11 @@
           </el-select>
         </div>
       </el-col>
+        <el-button type="primary" style="margin-top: 30px;width: 400px" >提交</el-button>
     </el-row>
+
+
+
   </div>
 </template>
 
@@ -27,11 +31,16 @@
           [{url: 'https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg', sign: null},
             {url: 'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg', sign: null},
             {url: 'http://ask.qcloudimg.com/article-cover-image/1065788/y98nuaaxl2.png', sign: null},
-            {url:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg'}],
+            {url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg'},
+            {url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg'},
+            {url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg'},
+            {url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg'},
+            {url: 'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3647170051,871438825&fm=26&gp=0.jpg'},],
         items: ['是', '不是'],
         value: ''
       };
-    }
+    },
+    methods: {}
 
   }
 </script>
